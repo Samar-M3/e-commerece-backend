bf4f377313cb3ce5ecad511c601db8f6c518aec8
Samar-M3 <gigachigga81@gmail.com>
2025-11-27 23:50:12 +0545

    again

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors=require("cors")
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.get("/",(req,res)=>{
     res.send('api is runnning');
 })
